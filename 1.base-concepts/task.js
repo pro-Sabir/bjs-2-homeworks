418d460cea8a38f400475f8d99d0b7bf7d970d9d
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-07-23 00:17:36 +0300

    Задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,62 +1,65 @@
 "use strict";
 
+// Функция для решения квадратных уравнений
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
 
-  if (discriminant < 0) {
-    // Нет корней
-    return arr;
-  } else if (discriminant === 0) {
-    // Один корень
-    let root = -b / (2 * a);
-    arr.push(root);
-    return arr;
-  } else {
-    // Два корня
-    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    arr.push(root1);
-    arr.push(root2);
-    return arr;
-  }
+	if (discriminant < 0) {
+		// Нет корней
+		return arr;
+	} else if (discriminant === 0) {
+		// Один корень
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else {
+		// Два корня
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1);
+		arr.push(root2);
+		return arr;
+	}
 }
 
 // Пример использования функции
 console.log(solveEquation(1, -3, 2)); // [2, 1]
 console.log(solveEquation(1, -2, 1)); // [1]
-console.log(solveEquation(1, 2, 3));  // []
+console.log(solveEquation(1, 2, 3)); // []
+
+"use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  // Проверка, что все аргументы являются числами
-  if (
-    typeof percent !== "number" ||
-    typeof contribution !== "number" ||
-    typeof amount !== "number" ||
-    typeof countMonths !== "number"
-  ) {
-    return false;
-  }
+	// Проверка, что все аргументы являются числами
+	if (
+		typeof percent !== "number" ||
+		typeof contribution !== "number" ||
+		typeof amount !== "number" ||
+		typeof countMonths !== "number"
+	) {
+		return false;
+	}
 
-  // Преобразование процентной ставки в диапазон от 0 до 1 и в месячную ставку
-  const monthlyPercent = percent / 100 / 12;
+	// Преобразование процентной ставки в диапазон от 0 до 1 и в месячную ставку
+	const monthlyPercent = percent / 100 / 12;
 
-  // Вычисление тела кредита
-  const creditBody = amount - contribution;
+	// Вычисление тела кредита
+	const creditBody = amount - contribution;
 
-  // Расчет ежемесячного платежа
-  const monthlyPayment =
-    creditBody *
-    (monthlyPercent +
-      monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
+	// Расчет ежемесячного платежа
+	const monthlyPayment =
+		creditBody *
+		(monthlyPercent +
+			monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1));
 
-  // Расчет общей суммы, которую придется заплатить клиенту
-  const totalPayment = monthlyPayment * countMonths;
+	// Расчет общей суммы, которую придется заплатить клиенту
+	const totalPayment = monthlyPayment * countMonths;
 
-  // Округление результата до двух значений после запятой
-  const roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+	// Округление результата до двух значений после запятой
+	const roundedTotalPayment = Math.round(totalPayment * 100) / 100;
 
-  return roundedTotalPayment;
+	return roundedTotalPayment;
 }
 
 // Тестовые случаи
@@ -67,4 +70,3 @@ console.log(calculateTotalMortgage(10, 1000, 20000, 24)); // 21042.09
 console.log(calculateTotalMortgage(10, 20000, 20000, 24)); // 0
 console.log(calculateTotalMortgage(10, 0, 10000, 36)); // 11616.19
 console.log(calculateTotalMortgage(15, 0, 10000, 36)); // 12479.52
-
