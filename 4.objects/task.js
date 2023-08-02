e3954bc7849f0e845f05ff8d0ab6e4c983b1c92e
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-02 13:32:44 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -20,7 +20,7 @@ Student.prototype.getAverage = function () {
   if (!this.marks || this.marks.length === 0) {
     return 0;
   }
-  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  const sum = this.marks.reduce((total, mark) => total + mark, 0);
   return sum / this.marks.length;
 };
 
@@ -30,4 +30,15 @@ Student.prototype.exclude = function (reason) {
   this.excluded = reason;
 };
 
-module.exports = Student;
+// Пример использования
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2);
