2161f2fd3071aa198d7259193ef7275a6d75bbec
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-02 12:54:05 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,17 +1,14 @@
-// Определяем функцию-конструктор Student
 function Student(name, gender, age) {
   this.name = name;
   this.gender = gender;
   this.age = age;
-  this.marks = []; // Инициализируем массив оценок пустым массивом
+  this.marks = [];
 }
 
-// Метод для установки предмета студенту
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 };
 
-// Метод для добавления оценок студенту
 Student.prototype.addMarks = function (...marksToAdd) {
   if (!this.marks) {
     this.marks = [];
@@ -19,7 +16,6 @@ Student.prototype.addMarks = function (...marksToAdd) {
   this.marks.push(...marksToAdd);
 };
 
-// Метод для получения среднего балла студента
 Student.prototype.getAverage = function () {
   if (!this.marks || this.marks.length === 0) {
     return 0;
@@ -28,19 +24,16 @@ Student.prototype.getAverage = function () {
   return sum / this.marks.length;
 };
 
-// Метод для исключения студента из учебного процесса
 Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
   this.excluded = reason;
 };
 
-// Экспортируем функцию-конструктор для использования в других файлах
 module.exports = {
   Student,
 };
 
-// Пример использования:
 
 // Создание студента
 const { Student } = require('./student');
