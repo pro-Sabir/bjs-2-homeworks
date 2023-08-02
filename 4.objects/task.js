58ae87a1cb25ba470c145d35c92c8b6ecba94278
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-02 13:17:05 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -32,7 +32,7 @@ Student.prototype.exclude = function (reason) {
 
 module.exports = Student;
 
-const { Student } = require('./task');
+const { Student } = require('./student'); 
 
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
