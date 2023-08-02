d317431ea64b367e984be27458226321ab602f6c
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-02 13:04:34 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,4 +1,9 @@
-
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+}
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
@@ -25,31 +30,18 @@ Student.prototype.exclude = function (reason) {
   this.excluded = reason;
 };
 
-module.exports = {
-  Student,
-};
-
-
 // Создание студента
-const { Student } = require('./task'); // Правильный путь к файлу task.js
 let student1 = new Student("Василиса", "женский", 19);
 student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
 console.log(student1.getAverage()); // 4.5
-console.log(student1); // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+console.log(student1);
 
 let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
-console.log(student2.getAverage()); // 0
-console.log(student2); // {name: "Артём", gender: "мужской", age: 25, subject: "Geometry"}
-
-student2.addMarks(3, 4, 5);
-console.log(student2.getAverage()); // 4
-console.log(student2); // {name: "Артём", gender: "мужской", age: 25, marks: [3, 4, 5], subject: "Geometry"}
-
-student2.exclude('плохая учёба')
-console.log(student2); // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
+student2.exclude('плохая учёба');
+console.log(student2);
 
 
 
