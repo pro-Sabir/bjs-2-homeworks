966adcb6e01a03571f7c92f8712854e084af3bbe
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-02 13:09:46 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -30,20 +30,6 @@ Student.prototype.exclude = function (reason) {
   this.excluded = reason;
 };
 
-// Создание студента
-const { Student } = require('./task'); // Правильный путь к файлу task.js
-
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
-console.log(student1);
-
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба');
-console.log(student2);
 
 
 
