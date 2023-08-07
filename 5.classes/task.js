9d46b0e04e427b5b592660fffa58b6ff527708d0
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-07 17:03:28 +0300

    Задание 5

---
+++ 5.classes/task.js
@@ -0,0 +1,87 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      this.name = name;
+      this.releaseDate = releaseDate;
+      this.pagesCount = pagesCount;
+      this.state = 100;
+      this.type = null;
+    }
+  
+    fix() {
+      this.state *= 1.5;
+    }
+  
+    set state(newState) {
+      if (newState < 0) {
+        this._state = 0;
+      } else if (newState > 100) {
+        this._state = 100;
+      } else {
+        this._state = newState;
+      }
+    }
+  
+    get state() {
+      return this._state;
+    }
+  }
+  
+  class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.type = "magazine";
+    }
+  }
+  
+  class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(name, releaseDate, pagesCount);
+      this.author = author;
+      this.type = "book";
+    }
+  }
+  
+  class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "novel";
+    }
+  }
+  
+  class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "fantastic";
+    }
+  }
+  
+  class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+      super(author, name, releaseDate, pagesCount);
+      this.type = "detective";
+    }
+  }
+  
+  const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+  );
+  
+  console.log(sherlock.releaseDate); // 2019
+  console.log(sherlock.state); // 100
+  sherlock.fix();
+  console.log(sherlock.state); // 150
+  
+  const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+  );
+  
+  console.log(picknick.author); // "Аркадий и Борис Стругацкие"
+  picknick.state = 10;
+  console.log(picknick.state); // 10
+  picknick.fix();
+  console.log(picknick.state); // 15
