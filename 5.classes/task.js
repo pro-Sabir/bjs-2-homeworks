1374033aef23a7dcc3bb95eab6efe9854f86b855
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-07 17:07:48 +0300

    Задание 5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -62,30 +62,6 @@ class PrintEditionItem {
     }
   }
   
-  const sherlock = new PrintEditionItem(
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
-  );
-  
-  console.log(sherlock.releaseDate); // 2019
-  console.log(sherlock.state); // 100
-  sherlock.fix();
-  console.log(sherlock.state); // 150
-  
-  const picknick = new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
-  );
-  
-  console.log(picknick.author); // "Аркадий и Борис Стругацкие"
-  picknick.state = 10;
-  console.log(picknick.state); // 10
-  picknick.fix();
-  console.log(picknick.state); // 15
-
   class Library {
     constructor(name) {
       this.name = name;
@@ -144,4 +120,3 @@ class PrintEditionItem {
   
   console.log(library.giveBookByName("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе"));
   console.log(library.books);
-  
