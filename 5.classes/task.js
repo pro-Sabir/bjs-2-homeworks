52ac6551bfac2270204d097c1b0d5e7ed679c441
Sabir Guseynov <freelanceguru.ru@gmail.com>
2023-08-07 17:21:50 +0300

    Задание 5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -95,28 +95,80 @@ class PrintEditionItem {
     }
   }
   
+  class Student {
+    constructor(name) {
+      this.name = name;
+      this.marks = {};
+    }
+  
+    addMark(mark, subject) {
+      if (mark >= 2 && mark <= 5) {
+        if (!this.marks[subject]) {
+          this.marks[subject] = [];
+        }
+        this.marks[subject].push(mark);
+      }
+    }
+  
+    getAverageBySubject(subject) {
+      const subjectMarks = this.marks[subject];
+      if (!subjectMarks || subjectMarks.length === 0) {
+        return 0;
+      }
+      const sum = subjectMarks.reduce((acc, mark) => acc + mark, 0);
+      return sum / subjectMarks.length;
+    }
+  
+    getAverage() {
+      const subjects = Object.keys(this.marks);
+      if (subjects.length === 0) {
+        return 0;
+      }
+      const totalSum = subjects.reduce((acc, subject) => {
+        const subjectAverage = this.getAverageBySubject(subject);
+        return acc + subjectAverage;
+      }, 0);
+      return totalSum / subjects.length;
+    }
+  }
+  
   // Пример использования
-  const library = new Library("Моя библиотека");
   
-  const sherlock = new PrintEditionItem(
-    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-    2019,
-    1008
+  const library = new Library("Библиотека имени Ленина");
+  
+  library.addBook(
+   new DetectiveBook(
+     "Артур Конан Дойл",
+     "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+     2019,
+     1008
+   )
   );
-  sherlock.fix();
-  library.addBook(sherlock);
-  
-  const picknick = new FantasticBook(
-    "Аркадий и Борис Стругацкие",
-    "Пикник на обочине",
-    1972,
-    168
+  library.addBook(
+   new FantasticBook(
+     "Аркадий и Борис Стругацкие",
+     "Пикник на обочине",
+     1972,
+     168
+   )
   );
-  picknick.state = 10;
-  library.addBook(picknick);
+  library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+  library.addBook(new Magazine("Мурзилка", 1924, 60));
   
-  console.log(library.findBookBy("name", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе"));
-  console.log(library.findBookBy("name", "Аркадий и Борис Стругацкие"));
+  console.log(library.findBookBy("name", "Властелин колец")); // null
+  console.log(library.findBookBy("releaseDate", 1924).name); // "Мурзилка"
   
-  console.log(library.giveBookByName("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе"));
-  console.log(library.books);
+  console.log("Количество книг до выдачи: " + library.books.length); // Количество книг до выдачи: 4
+  library.giveBookByName("Машина времени");
+  console.log("Количество книг после выдачи: " + library.books.length); // Количество книг после выдачи: 3
+  
+  const student = new Student("Олег Никифоров");
+  student.addMark(5, "химия");
+  student.addMark(5, "химия");
+  student.addMark(5, "физика");
+  student.addMark(4, "физика");
+  student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+  
+  console.log(`Средний балл по предмету физика: ${student.getAverageBySubject("физика")}`);
+  console.log(`Средний балл по предмету биология: ${student.getAverageBySubject("биология")}`);
+  console.log(`Средний балл по всем предметам: ${student.getAverage()}`);
